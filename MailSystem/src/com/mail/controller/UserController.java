e4f7b5516edaa63db46c781a14b2db81bc16c04f
clearlove79 <49556327+clearlove79@users.noreply.github.com>
2019-04-14 23:50:01 +0800

    Add files via upload

--- MailSystem/src/com/mail/controller/UserController.java
+++ MailSystem/src/com/mail/controller/UserController.java
@@ -9,8 +9,11 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mail.pojo.User;
 import com.mail.service.UserService;
 import com.mail.util.PassUtil;
@@ -23,9 +26,11 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
+	private Gson gson = new GsonBuilder().serializeNulls().create();
+	
 	@RequestMapping("listUser")
 	public ModelAndView listUser(int page) {
-		Map map =new HashMap();
+		Map<String,Object> map = new HashMap<String,Object>();
 		int limit_size=20;
 		int limit_start=page*limit_size;
 		map.put("limit_start", limit_start);
@@ -39,7 +44,7 @@ public class UserController {
 	
 	@RequestMapping(value = "addUser",produces = "text/plain;charset=utf-8")
 	public ModelAndView addUser(String username,String password) throws NoSuchAlgorithmException {
-		Map map = new HashMap();
+		Map<String,Object> map = new HashMap<String,Object>();
 		Set<String> sensitiveWord = userService.getSensitiveWordSet();
 		/*SensitiveWordUtil wordUtil = new SensitiveWordUtil();
 		wordUtil.initSensitiveWordsMap(sensitiveWord);
@@ -78,17 +83,25 @@ public class UserController {
 	}
 	//后台登录
 	@RequestMapping("AdminLogin")
+	@ResponseBody
 	public String AdminLogin(String username, String pass){
+		System.out.print(username);
 		User Auser = this.userService.getUserByUserid(username);
+		String result ="";
+		Map<String,Object> map = new HashMap<String,Object>();
 		if(Auser==null)
-			return "00"; //没有该用户
+			result = "00"; //没有该用户
 		else {
 			if(Auser.getPassword().equals(pass))
-				return "1";  //登录
+				result="1";  //登录
 			else {
-				return "01";  //密码错误
+				result="01";  //密码错误
 			}
 		}
+		map.put("result", result);
+		System.out.print(map);
+		return gson.toJson(map);
 	}
 	
 }
+
